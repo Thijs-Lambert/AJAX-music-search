e7d5c473f8ea2b11e16bf38a0f0e5a1230b0bf6e
Thijs Lambert <thijs.lamb@gmail.com>
2018-11-12 11:48:12 +0100

    styling and autocomplete search

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-{
+{   
+
+    const createResultList = (results) => {
+        const $results = document.querySelector(`.results`);
+        $results.innerHTML = results.map(createResultListItem).join(``);
+    }
+
+    const createResultListItem = result => `<li class="result" id="${result.id}">${result.name}</li>`;
+
+
+
+
+    const parse = (results) => {
+        createResultList(results);
+    };
 
     const search = value => {
-        const url = ` `;
+        const url = `https://musicdemons.com/api/v1/artist/autocomplete`;
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+              },
+            body: `name=${value}`
+        };
 
+        fetch(url, options)
+        .then(response => response.json())
+        .then(jsonData => parse(jsonData));
     };
 
     const handleKeyUpSearch = e => {
-        if (e.keyCode === 13) {
-            const $input = e.currentTarget;
-            search($input.value);
-        }
+        const $input = e.currentTarget;
+        search($input.value);
     };
 
     const init = () => {
